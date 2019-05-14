1eeea47d96e06078b15365351e0edb797301b46c
Beto Dute <betodute@gmail.com>
2019-05-14 22:44:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,17 +10,17 @@ function nowServing(katzDeliLine) {
   if (katzDeliLine.length > 0) {
   return `Currently serving ${katzDeliLine.shift()}.`;
   } else {
- return "There is nobody waiting to be served!"; 
-}
+  return "There is nobody waiting to be served!"; 
+  }
 }
 
 function currentLine(katzDeliLine) {
   var tempLine = [];
   if (katzDeliLine > tempLine) {
-   for (var i=0; i<katzDeliLine.length; i++) {
+    for (var i=0; i<katzDeliLine.length; i++) {
      tempLine.push(` ${i+1}. ${katzDeliLine[i]}`);
-   }
-   return "The line is currently:" +;
+     return "The line is currently"
+    }
   } else {
    return "The line is currently empty.";
   }
